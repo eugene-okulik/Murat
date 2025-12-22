d6129a9c9923e3049041121bde2e6351fd24acb8
Murat.Baidauletov <murat.baidauletov@medico.kz>
2025-12-22 16:01:58 +0500

    homework_5 done

---
+++ homework/murat_baidauletov/Homework_5/task2.py
@@ -0,0 +1,12 @@
+text = "результат операции: 42"
+start_number_index = text.index(':') + 2
+numb = int(text[start_number_index:])
+print(numb + 10)
+text = "результат операции: 514"
+start_number_index = text.index(':') + 2
+numb = int(text[start_number_index:])
+print(numb + 10)
+text = "результат работы программы: 9"
+start_number_index = text.index(':') + 2
+numb = int(text[start_number_index:])
+print(numb + 10)
---
+++ homework/murat_baidauletov/Homework_5/task3.py
@@ -0,0 +1,6 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+
+subjects = ['math', 'biology', 'geography']
+
+print('Students',', ' .join(students),'study these subjects:', ', '.join(subjects))
+
