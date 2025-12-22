dac1e8ba2592ef5af7b69b30898f67cab0dc0a10
Murat.Baidauletov <murat.baidauletov@medico.kz>
2025-12-22 16:08:37 +0500

    fixed formatting

--- homework/murat_baidauletov/Homework_5/task3.py
+++ homework/murat_baidauletov/Homework_5/task3.py
@@ -2,5 +2,4 @@ students = ['Ivanov', 'Petrov', 'Sidorov']
 
 subjects = ['math', 'biology', 'geography']
 
-print('Students',', ' .join(students),'study these subjects:', ', '.join(subjects))
-
+print('Students', ', '.join(students), 'study these subjects:', ', '.join(subjects))
