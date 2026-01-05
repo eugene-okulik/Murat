ac616447d76b49580a7bebe552ec7067f6efb7a6
Murat.Baidauletov <murat.baidauletov@medico.kz>
2026-01-05 10:51:17 +0500

    fixed formating

--- homework/murat_baidauletov/Homework_8/task1.py
+++ homework/murat_baidauletov/Homework_8/task1.py
@@ -1,11 +1,11 @@
 from random import choice, randint
 
-bonus = ['True', 'False']
+bonus = [True, False]
 
 while True:
     salary = int(input('enter salary: '))
     bonus = choice([True, False])
-    if bonus == True:
+    if bonus:
         salary_bonus = salary + randint(0, 1000)
         print(f"{salary}, {bonus} - '${salary_bonus}'")
     else:
