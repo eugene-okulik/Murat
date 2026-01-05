1915e5d0ed77ce936f6b6bdba0bbcb9a8f875d69
Murat.Baidauletov <murat.baidauletov@medico.kz>
2026-01-05 10:35:23 +0500

    Homework_8 done

---
+++ homework/murat_baidauletov/Homework_8/task1.py
@@ -0,0 +1,12 @@
+from random import choice, randint
+
+bonus = ['True', 'False']
+
+while True:
+    salary = int(input('enter salary: '))
+    bonus = choice(['True', 'False'])
+    if bonus == 'True':
+        salary_bonus = salary + randint(0,10000)
+        print(f"{salary},{bonus} - '${salary_bonus}'")
+    else:
+        print(f"{salary},{bonus} - '${salary}'")
---
+++ homework/murat_baidauletov/Homework_8/task2.py
@@ -0,0 +1,18 @@
+# Ограничили количество шагов до 100,чтобы случайно не запустить бесконечный цикл.
+
+def fibonachi(limit = 100):
+    start_num1 = 0
+    start_num2 = 1
+    count = 1
+    while count < limit:
+        yield start_num1
+        start_num1, start_num2 = start_num2, start_num1 + start_num2
+        count += 1
+
+num = 0
+for number in fibonachi(1000000):
+    num += 1
+    if num == 5 or num == 200 or num == 1000 or num == 100000:
+        print(number)
+        if num == 100000:
+            break
