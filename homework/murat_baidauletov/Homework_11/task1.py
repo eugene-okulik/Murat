455aecb9ddd4adb641e950ed726b1c301ab2f867
Murke1998 <murat.baydauletov.98@mail.ru>
2026-06-10 16:57:13 +0500

    Исправлены замечания по Book и SchoolBook

--- homework/murat_baidauletov/Homework_11/task1.py
+++ homework/murat_baidauletov/Homework_11/task1.py
@@ -1,23 +1,22 @@
 class Book:
+    page_material = 'Бумага'
+    text = True
+
     def __init__(
         self,
-        page_material,
-        text,
         title,
         author,
         page_count,
         isbn,
         reserved=False
     ):
-        self.page_material = page_material
-        self.text = text
         self.title = title
         self.author = author
         self.page_count = page_count
         self.isbn = isbn
         self.reserved = reserved
 
-    def reserved_book(self):
+    def show_book_info(self):
         if self.reserved:
             print(
                 f"Название: {self.title}, Автор: {self.author}, "
@@ -30,24 +29,84 @@ class Book:
                 f"страниц: {self.page_count}, материал: {self.page_material}"
             )
 
+class SchoolBook(Book):
+    def __init__(
+        self,
+        title,
+        author,
+        page_count,
+        isbn,
+        subject,
+        school_class,
+        tasks,
+        reserved=False
+    ):
+        super().__init__(
+            title,
+            author,
+            page_count,
+            isbn,
+            reserved
+        )
+        self.subject = subject
+        self.school_class = school_class
+        self.tasks = tasks
 
-reserved_book1 = Book(
-    "бумага",
+    def show_book_info(self):
+        if self.reserved:
+            print(
+                f"Название: {self.title}, Автор: {self.author}, "
+                f"страниц: {self.page_count}, предмет: {self.subject}, "
+                f"класс: {self.school_class}, зарезервирована"
+            )
+        else:
+            print(
+                f"Название: {self.title}, Автор: {self.author}, "
+                f"страниц: {self.page_count}, предмет: {self.subject}, "
+                f"класс: {self.school_class}"
+            )
+
+#add book1
+book1 = Book(
+    "Тест",
+    "тест123",
+    200,
+    "458"
+)
+
+#reserved book1
+book1.reserved = True
+book1.show_book_info()
+
+#add book2
+book2 = Book(
+    "Война и мир",
+    "Мурат murat",
+    "200",
+    1200,
+
+)
+book2.show_book_info()
+
+school_book1 = SchoolBook(
+    "Алгебра",
+    "Иванов",
+    200,
+    "123",
+    "Математика",
+    9,
     True,
-    "Идиот",
-    "Достоевский",
-    500,
-    "456",
     reserved=True
 )
-reserved_book1.reserved_book()
+school_book1.show_book_info()
 
-not_reserved_book2 = Book(
-    "бумага",
-    True,
-    "Война и мир",
-    "Толстой",
-    1200,
-    "789"
+school_book2 = SchoolBook(
+    "История Казахстана",
+    "Ақтөре",
+    180,
+    "456",
+    "История",
+    7,
+    True
 )
-not_reserved_book2.reserved_book()
+school_book2.show_book_info()
